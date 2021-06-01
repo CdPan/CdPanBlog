72899e3cba75612c573c24695c4c2ebd49475111
CdPan <1367134373@qq.com>
2021-06-01 17:45:45 +0800

    提交所有文件

---
+++ blog/src/main/java/com/pan/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.pan.dao;
+
+import com.pan.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface UserDao {
+    User findByUsernameAndPassword(@Param("username") String username,@Param("password") String password);
+
+}
